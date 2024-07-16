1da04dc37663ab7660499cf8520f83d4e8d645c5
Char2sGu <char2s@outlook.com>
2024-07-16 21:07:00 +0800

    init workspace

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'adx-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
