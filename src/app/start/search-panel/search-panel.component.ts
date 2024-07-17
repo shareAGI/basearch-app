6825e1705db0bbe4e657a4dffa25e257934839e1
Char2sGu <char2s@outlook.com>
2024-07-18 04:05:34 +0800

    feat: implement simple search result card

--- src/app/start/search-panel/search-panel.component.ts
+++ src/app/start/search-panel/search-panel.component.ts
@@ -9,6 +9,7 @@ import {
   SearchTrailingSlot,
 } from '../../shared/search/search.component';
 import { SearchControlComponent } from '../../shared/search-control/search-control.component';
+import { SearchResultCardComponent } from '../search-result-card/search-result-card.component';
 
 @Component({
   selector: 'adx-search-panel',
@@ -19,6 +20,7 @@ import { SearchControlComponent } from '../../shared/search-control/search-contr
     [SearchComponent, SearchTrailingSlot, SearchControlComponent],
     IconComponent,
     IconButtonComponent,
+    SearchResultCardComponent,
   ],
   templateUrl: './search-panel.component.html',
   styleUrl: './search-panel.component.scss',
@@ -26,8 +28,16 @@ import { SearchControlComponent } from '../../shared/search-control/search-contr
 export class SearchPanelComponent {
   private formBuilder = inject(FormBuilder).nonNullable;
 
-  keywordsInitial = input.required<string>();
+  keywordsInitial = input.required<string>({ alias: 'keywords' });
   keywordsControl = computed(() =>
     this.formBuilder.control(this.keywordsInitial()),
   );
+
+  card = {
+    headline: 'Headline',
+    url: 'https://domain.com/asdfasdfasdf.html',
+    cover: 'https://domain.com/asdfasdfasdf.jpg',
+    summary: 'adsfasfasdasdf',
+  };
+  cards = [this.card, this.card, this.card, this.card, this.card, this.card];
 }
--- src/app/start/search-result-card/search-result-card.component.ts
+++ src/app/start/search-result-card/search-result-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 
 @Component({
   selector: 'adx-search-result-card',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './search-result-card.component.html',
   styleUrl: './search-result-card.component.scss',
+  host: {
+    'class': 'group',
+    '[class.expanded]': 'expanded()',
+  },
 })
-export class SearchResultCardComponent {}
+export class SearchResultCardComponent {
+  headline = input.required<string>();
+  cover = input.required<string>();
+  summary = input.required<string>();
+  url = input.required<string>();
+  urlPrettified = computed(() => {
+    const url = this.url();
+    const urlObject = new URL(url);
+    return urlObject.hostname + urlObject.pathname;
+  });
+
+  expanded = signal(false);
+}
