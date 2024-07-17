9730398c718d81a96ffa2b59c0ddd8aee1f402a0
Char2sGu <char2s@outlook.com>
2024-07-18 02:47:16 +0800

    feat: optimize styles

---
+++ src/app/start/search-result-card/search-result-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'adx-search-result-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-result-card.component.html',
+  styleUrl: './search-result-card.component.scss',
+})
+export class SearchResultCardComponent {}
